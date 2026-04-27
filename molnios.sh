1c4c0555bb3bc62ba71825c2ca45b0cce566e95b
al1h3n <al1h3n@molni.com>
2026-04-27 18:47:00 +0500

    Fix

--- molnios.sh
+++ molnios.sh
@@ -804,7 +804,7 @@ install(){
         git -C $SHARED_NIX_PATH update-index --assume-unchanged configuration.nix
 
         cd $SHARED_PATH&&git add .
-        read -p $'${YELLOW}Adjust your modules configuration now and then hit enter.${RESET}'
+        echo -ne "${YELLOW}Adjust your modules configuration now and then hit enter.${RESET} "&&read
         nixos-rebuild switch --impure --upgrade --flake $SHARED_NIX_PATH#main
         # ! Dirty git tree - isn't a problem. It happens if you don't commit changes.
 
